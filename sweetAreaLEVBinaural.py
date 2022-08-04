32886f7a71fa9840386abe282c382c1a12fd3aa1
Stefan Riedel <35775985+stefanriedel@users.noreply.github.com>
2022-08-04 18:15:31 +0200

    Update sweetAreaLEVBinaural.py

--- sweetAreaLEVBinaural.py
+++ sweetAreaLEVBinaural.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     RECTANGULAR_ARRAY = False
     if not RECTANGULAR_ARRAY:
         # get coordinates for circular array with nLS sources
-        nLS = 8
+        nLS = 4
         x_ls, y_ls, phi_ls = getCircularArray(nLS=nLS, offset_degree=45)
     else:
         # get coordinates for rectangular array
@@ -155,6 +155,128 @@ if __name__ == '__main__':
     LEV_ILD = np.reshape(LEV_ILD, (num_source_models, res,res))
     LEV_IC = np.reshape(LEV_IC, (num_source_models, res,res))
 
+
+    scale = 0.8
+    fig, axes = plt.subplots(nrows=2, ncols=num_source_models, figsize=(12*scale,8*scale), sharex=True, sharey=True)
+    fig.subplots_adjust(wspace=0.05, hspace=0.05)
+
+    ILD_lvls = np.array([-10,-6,-3,-1,0])
+    IC_lvls = np.array([0.0, 0.2, 0.4, 0.6, 1.0])
+
+    LEV_ILD = np.clip(LEV_ILD, a_min=-9.9, a_max=0.0)
+
+    font_sz = 18
+
+    cbar_ticklabels = [['10','6','3','1','0'],  ['1.0', '0.8' , '0.6', '0.4', '0.0'] ]
+    #cbar_ticklabels = [['10.0','6.0','3.0','1.0','0.0'],  ['1.0', '0.8' , '0.6', '0.4', '0.0'] ]
+
+    #cbar_pos_size = [[0.92, 0.51, 0.01, 0.37], [0.92, 0.11, 0.01, 0.37]]
+    cbar_pos_size = [[0.1, 0.54, 0.01, 0.30], [0.1, 0.15, 0.01, 0.30]]
+
+    bmap = colors.ListedColormap(["black", "black", "black", "black"])
+
+    titles = [ 'const. sources', 'line sources', 'point sources']
+
+    vmins = [-10, 0.0]
+    vmaxs = [0.0, 1.0]
+
+    #clrs = ['0.25', '0.5', '0.75', '1.0']
+    import matplotlib
+    cmap = matplotlib.cm.get_cmap('BuPu_r')
+    #clrs = [cmap(0.4), cmap(0.6), cmap(0.8), cmap(1.0)]
+    clrs = [cmap(0.5), cmap(0.6), cmap(0.8), '1.0']
+
+    for r in range(2):
+        for c in range(num_source_models):
+            if r == 0:
+                LEV = LEV_ILD[c,:]
+                lvls = ILD_lvls
+                axes[r,c].set_title(titles[c], fontsize=font_sz)
+                if c == 0:
+                    axes[r,c].set_ylabel('ILD in dB', fontsize=font_sz, labelpad=48)
+                    #axes[r,c].yaxis.set_label_position("right")
+            if r == 1:
+                LEV = LEV_IC[c,:]
+                lvls = IC_lvls
+                if c == 0:
+                    axes[r,c].set_ylabel('IC', fontsize=font_sz, labelpad=48)
+                    #axes[r,c].yaxis.set_label_position("right")
+            #pcm = axes[r,c].contourf(listener_X,listener_Y, LEV, levels=lvls, colors=clrs, zorder=1, vmin=vmins[r], vmax=vmaxs[r])
+            pcm = axes[r,c].contourf(listener_X,listener_Y, LEV, levels=lvls, colors=clrs, zorder=1, vmin=vmins[r], vmax=vmaxs[r])
+
+            if c == num_source_models-1:
+                color_bar_ax = fig.add_axes(cbar_pos_size[r])
+                cbar = fig.colorbar(pcm, cax=color_bar_ax)
+                cbar.ax.set_yticklabels(cbar_ticklabels[r])
+                cbar.ax.tick_params(labelsize=12)
+                cbar.ax.yaxis.set_ticks_position('left')
+
+            axes[r,c].contour(listener_X,listener_Y, LEV, levels=lvls, cmap=bmap, linewidths=0.5, zorder=2)
+            axes[r,c].set_xlim(-area_len, area_len)
+            axes[r,c].set_ylim(-area_len, area_len)
+            #axes[r,c].set_xticks([-1.0,-0.5,0,0.5,1.0],['-1.0','-0.5','0.0','0.5','1.0'], fontsize=8)
+            #axes[r,c].set_yticks([-1.0,-0.5,0,0.5,1.0],['-1.0','-0.5','0.0','0.5','1.0'], fontsize=8)
+            axes[r,c].set_xticks([-1.0,-0.5,0,0.5,1.0])
+            axes[r,c].set_yticks([-1.0,-0.5,0,0.5,1.0])
+            axes[r,c].grid(visible=True, alpha=0.2)
+            axes[r,c].yaxis.set_ticks_position("right")
+
+            # Marker for on-center and off-center position of experiment
+            axes[r,c].scatter(0,0,s=30,marker='x', edgecolors='k', c='k')
+            axes[r,c].scatter(0.5,0,s=30,marker='x', edgecolors='k', c='k')
+
+            
+            # Source / Loudspeaker icon drawing
+            t = np.arange(1/8, 1, 1/4) * 2*np.pi
+            x_square = np.cos(t) * 0.1
+            y_square = np.sin(t) * 0.1
+            x_tri = np.array([0,1,-1]) * 0.15
+            y_tri = np.array([0,1,1]) * 0.15
+            square = np.array([x_square, y_square]).transpose()
+            tri = np.array([x_tri, y_tri]).transpose()
+
+            sz = 0.5
+            square = square * sz
+            tri = tri * sz
+
+            phi = phi_ls / 180 * np.pi
+            ls = np.array([x_ls,y_ls]).transpose()
+            #tr = ls_coord
+            alpha = -np.pi/2 - phi
+
+            if RECTANGULAR_ARRAY:
+                alpha = -np.pi/2 - draw_rot
+
+            rot_mat = np.zeros((phi.shape[0], 2, 2))
+            for n in range(phi.shape[0]):
+                rot_mat[n,:,:] = np.array([[np.cos(alpha[n]),-np.sin(alpha[n])],[np.sin(alpha[n]), np.cos(alpha[n])]])
+
+            squares = np.zeros((phi.shape[0],4,2))
+            tris = np.zeros((phi.shape[0],3,2))
+
+            for n in range(phi.shape[0]):
+                squares[n,:,:] = np.dot(square, rot_mat[n,:,:])
+                tris[n,:,:] = np.dot(tri, rot_mat[n,:,:])
+
+            for n in range(phi.shape[0]):
+                axes[r,c].fill(tris[n,:,0] + ls[n,0] , tris[n,:,1] + ls[n,1], color='w', zorder=3, edgecolor='k')
+                axes[r,c].fill(squares[n,:,0] + ls[n,0] , squares[n,:,1] + ls[n,1], color='w', zorder=4, edgecolor='k')
+
+    if USE_GAMMATONE_WINDOWS:
+        add_string = '_ERB'
+    else:
+        add_string = ''
+
+    if not RECTANGULAR_ARRAY:
+        plt.savefig(fname=pjoin(save_path, str(nLS) + 'LS_sweet_area' + add_string + '.pdf'), bbox_inches='tight')
+    else:
+        plt.savefig(fname=pjoin(save_path, 'RECT_' + str(int(array_width*100)) + 'wide' + 
+        str(int(array_length*100)) + 'long_' + str(phi_ls.size) + 'LS_sweet_area' + add_string + '.pdf'), bbox_inches='tight')
+
+
+
+
+    """
     # Plotting the computed values
     scale = 0.8
     fig, axes = plt.subplots(nrows=2, ncols=num_source_models, figsize=(12*scale,8*scale))
@@ -203,8 +325,8 @@ if __name__ == '__main__':
             axes[r,c].contour(listener_X,listener_Y, LEV, levels=lvls, cmap=bmap, linewidths=0.5, zorder=2)
             axes[r,c].set_xlim(-area_len, area_len)
             axes[r,c].set_ylim(-area_len, area_len)
-            axes[r,c].set_xticks([])
-            axes[r,c].set_yticks([])
+            axes[r,c].set_xticks([-1.0,-0.5,0,0.5,1.0])
+            axes[r,c].set_yticks([-1.0,-0.5,0,0.5,1.0])
             
             # Source / Loudspeaker icon drawing
             t = np.arange(1/8, 1, 1/4) * 2*np.pi
@@ -252,4 +374,5 @@ if __name__ == '__main__':
     else:
         plt.savefig(fname=pjoin(save_path, 'RECT_' + str(int(array_width*100)) + 'wide' + 
         str(int(array_length*100)) + 'long_' + str(phi_ls.size) + 'LS_sweet_area' + add_string + '.pdf'), bbox_inches='tight')
+    """
 
